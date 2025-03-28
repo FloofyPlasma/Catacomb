ca8f05d3925cc6dc10fee1b56c8bb2040e9a2510
FloofyPlasma <github@floofyplasma.com>
2025-03-27 23:57:59 -0400

    feat: particle

--- Catacomb/Entity/Particle/Particle.cs
+++ Catacomb/Entity/Particle/Particle.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Catacomb.Screen;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,52 @@ namespace Catacomb.Entity.Particle
 {
     public class Particle : Entity
     {
+        public double xa, ya, za;
+        public double z;
+        public Entity owner;
+        public int life;
+
+        public Particle(double x, double y, double xa, double ya)
+        {
+            pos.Set(x, y);
+            double pow = new Random().NextDouble() * 1 + 1; // TODO: network synced random
+            this.xa = xa * pow;
+            this.ya = ya * pow;
+            za = new Random().NextDouble() * 2 + 1.0; // TODO: network synced random
+            SetSize(2, 2);
+            physicsSlide = false;
+            isBlocking = false;
+            life = new Random().Next(20) + 50; // TODO: network synced random
+        }
+
+        public override void Tick() // FIXME: Should this be override>
+        {
+            Move(xa, ya);
+            z += za;
+
+            if (z < 0)
+            {
+                z = 0;
+                xa *= 0.8;
+                ya *= 0.8;
+            }
+            else
+            {
+                xa *= 0.98;
+                ya *= 0.98;
+            }
+
+            za -= 0.2;
+
+            if (--life < 0)
+            {
+                Remove();
+            }
+        }
+
+        public override void Render(IAbstractScreen screen)
+        {
+           // TODO: render to screen
+        }
     }
 }

a7766e47e2b6f91e4310c6274160f33af8322e1d
FloofyPlasma <github@floofyplasma.com>
2025-03-27 23:41:01 -0400

    feat: melee bullet

--- Catacomb/Entity/BulletMelee.cs
+++ Catacomb/Entity/BulletMelee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Catacomb.Screen;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,33 @@ namespace Catacomb.Entity
 {
     public class BulletMelee : Bullet
     {
+        public uint color = 0xffcc9966;
+
+        public BulletMelee(Mob.Mob e, double xa, double ya, float damage, int range) : base(e, xa, ya, damage)
+        {
+            duration = range;
+        }
+
+        public BulletMelee(Mob.Mob e, double xa, double ya, float damage, int range, uint color) : this(e, xa, ya, damage, range)
+        {
+            this.color = color;
+        }
+
+        public override void Tick()
+        {
+            if (--duration <= 0)
+            {
+                Remove();
+
+                return;
+            }
+
+            base.Tick();
+        }
+
+        public override void Render(IAbstractScreen screen)
+        {
+            // TODO: render to screen
+        }
     }
 }

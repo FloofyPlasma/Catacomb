bfad054ed5ef00fa41183e8279ff7b3053977fa7
FloofyPlasma <github@floofyplasma.com>
2025-03-27 23:43:30 -0400

    feat: bullet poison

--- Catacomb/Entity/BulletPoison.cs
+++ Catacomb/Entity/BulletPoison.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Catacomb.Screen;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,34 @@ namespace Catacomb.Entity
 {
     public class BulletPoison : Bullet
     {
+        public BulletPoison(Mob.Mob e, double xa, double ya, float damage) : base(e, xa, ya, damage)
+        {
+            duration = 80;
+            this.xa = xa * 3;
+            this.ya = ya * 3;
+        }
+
+        public override void Collide(Entity entity, double xa, double ya)
+        {
+            if (entity is Mob.Mob)
+            {
+                Mob.Mob mobEnt = (Mob.Mob)entity;
+
+                if (mobEnt.IsNotFriendOf(owner) && !(entity is Building.Building))
+                {
+                    mobEnt.Hurt(this, damage);
+                    hit = true;
+                }
+            }
+
+            if (hit)
+            {
+                // TODO: play sound
+            }
+        }
+        public override void Render(IAbstractScreen screen)
+        {
+            // TODO: render to screen
+        }
     }
 }

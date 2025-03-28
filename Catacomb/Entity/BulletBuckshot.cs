4387b04c147c8b93eaf73ca401c142715fed474f
FloofyPlasma <github@floofyplasma.com>
2025-03-27 23:17:33 -0400

    feat: buckshot

--- Catacomb/Entity/BulletBuckshot.cs
+++ Catacomb/Entity/BulletBuckshot.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Catacomb.Screen;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,46 @@ namespace Catacomb.Entity
 {
     public class BulletBuckshot : Bullet
     {
+        public BulletBuckshot(Mob.Mob owner, double xa, double ya, float damage) : base(owner, xa, ya, damage)
+        {
+            this.owner = owner;
+            pos.Set(owner.pos.x + xa * 4, owner.pos.y + ya * 4);
+            this.xa = xa * 18;
+            this.ya = ya * 18;
+            SetSize(2, 2);
+            physicsSlide = false;
+            duration = 20;
+            this.damage = damage;
+        }
+
+        override public void Tick()
+        {
+            if (--duration <= 0)
+            {
+                Remove();
+
+                return;
+            }
+
+            if (!Move(xa, ya))
+            {
+                hit = true;
+            }
+
+            if (hit && !removed)
+            {
+                Remove();
+            }
+
+            if (damage > 0.5f)
+            {
+                damage -= 0.5f;
+            }
+        }
+
+        public override void Render(IAbstractScreen screen)
+        {
+            // TODO: render to screen
+        }
     }
 }

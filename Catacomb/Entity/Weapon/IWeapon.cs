0fe5e361352d5cf03fa6073744a76fc8a9e4d1fb
FloofyPlasma <github@floofyplasma.com>
2025-03-28 00:01:08 -0400

    feat: weapon interface

--- Catacomb/Entity/Weapon/IWeapon.cs
+++ Catacomb/Entity/Weapon/IWeapon.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Catacomb.Screen;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Catacomb.Entity.Weapon
 {
     public interface IWeapon
     {
+        void SetOwner(Mob.Mob mob);
+        void WeaponTick();
+        void UpgradeWeapon();
+        void PrimaryFire(double xDir, double yDir);
+        Bullet GetAmmo(double xDir, double yDir);
+        public IAbstractBitmap GetSprite();
+        void SetWeaponMode();
     }
 }

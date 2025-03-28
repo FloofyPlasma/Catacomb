ca911115778cbbd347c41ef4b39f16312b296bcb
FloofyPlasma <github@floofyplasma.com>
2025-03-27 23:51:29 -0400

    feat: entity intersects bounding box

--- Catacomb/Entity/Predicates/EntityIntersectsBB.cs
+++ Catacomb/Entity/Predicates/EntityIntersectsBB.cs
@@ -9,9 +9,16 @@ namespace Catacomb.Entity.Predicates
 {
     public sealed class EntityIntersectsBB : IBBPredicate<Entity>
     {
+        public static readonly EntityIntersectsBB Instance = new EntityIntersectsBB();
+
+        private EntityIntersectsBB() { }
         public bool AppliesTo(Entity item, double x0, double y0, double x1, double y1)
         {
-            throw new NotImplementedException();
+            return !item.removed && item.Intersects(x0, y0, x1, y1);
+        }
+        public override string ToString()
+        {
+            return GetType().Name;
         }
     }
 }

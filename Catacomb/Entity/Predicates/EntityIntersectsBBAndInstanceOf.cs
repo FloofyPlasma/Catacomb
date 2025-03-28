51a10aa18730ff9beb5bd5e969cc5e431b5c74a2
FloofyPlasma <github@floofyplasma.com>
2025-03-27 23:54:30 -0400

    feat: entity intersects bounding  box and instance of

--- Catacomb/Entity/Predicates/EntityIntersectsBBAndInstanceOf.cs
+++ Catacomb/Entity/Predicates/EntityIntersectsBBAndInstanceOf.cs
@@ -9,9 +9,32 @@ namespace Catacomb.Entity.Predicates
 {
     public class EntityIntersectsBBAndInstanceOf : IBBPredicate<Entity>
     {
+        private static HashSet<Type> entityClasses;
+        public EntityIntersectsBBAndInstanceOf(Type class1)
+        {
+            entityClasses = new HashSet<Type> { class1 };
+        }
+
+        public EntityIntersectsBBAndInstanceOf(Type class1, Type class2) : this(class1)
+        {
+            entityClasses.Add(class2);
+        }
+
         public bool AppliesTo(Entity item, double x0, double y0, double x1, double y1)
         {
-            throw new NotImplementedException();
+            foreach (var entityClass in entityClasses)
+            {
+                if (entityClass.IsInstanceOfType(item))
+                {
+                    return EntityIntersectsBB.Instance.AppliesTo(item, x0, y0, x1, y1);
+                }
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"EntityIntersectsOneOfBBPredicate{{ entityClasses={string.Join(", ", entityClasses)} }}";
         }
     }
 }

9e6a36ae0e894eff83a2d917aa94859d54db3e69
FloofyPlasma <github@floofyplasma.com>
2025-03-27 23:59:57 -0400

    feat: sparkle

--- Catacomb/Entity/Particle/Sparkle.cs
+++ Catacomb/Entity/Particle/Sparkle.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Catacomb.Screen;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace Catacomb.Entity.Particle
 {
     public class Sparkle : Particle
     {
+        public int duration;
+
+        public Sparkle(double x, double y, double xa, double ya) : base(x, y, xa, ya)
+        {
+            duration = (life = new Random().Next(10) + 20) + 1; // TODO: network synced random
+        }
+
+        public override void Render(IAbstractScreen screen)
+        {
+            // TODO: render to screen
+        }
     }
 }
